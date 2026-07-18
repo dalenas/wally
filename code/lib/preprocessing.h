b419696431c8c6e2c517afba34c1998f9604a9d9
Dalen Smith <dalens@uci.edu>
2026-07-18 16:27:19 -0700

    Starter declarations for preprocessing class. Forgot to save before commit.

--- code/lib/preprocessing.h
+++ code/lib/preprocessing.h
@@ -4,7 +4,21 @@
 #include "mlearn.h"
 
 class Preprocessing {
-    
-}
+private:
+
+public:
+    double mean(std::vector<double>);
+    std::vector<double> mean(std::vector<std::vector<double>>);
+    double var(std::vector<double>);
+    std::vector<double> var(std::vector<std::vector<double>>);
+    double stdd(std::vector<double>);
+    std::vector<double> stdd(std::vector<std::vector<double>>);
+    double cov(std::vector<double>, std::vector<double>);
+    std::vector<double> cov(std::vector<std::vector<double>>, std::vector<std::vector<double>>);
+    double corr(std::vector<double>, std::vector<double>);
+    std::vector<double> corr(std::vector<std::vector<double>>, std::vector<std::vector<double>>);
+    double norm(std::vector<double>);
+    std::vector<double> norm(std::vector<std::vector<double>>);
+};
 
 #endif
