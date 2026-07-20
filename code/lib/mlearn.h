590002dab59dfe946f3e6d20b2ea49e817df9e06
Dalen Smith <dalens@uci.edu>
2026-07-20 16:54:14 -0700

    Small changes to base class virtual methods.

--- code/lib/mlearn.h
+++ code/lib/mlearn.h
@@ -10,15 +10,23 @@ using matrix = std::vector<std::vector<double>>;
 class Regressor {
 private:
     using targets = std::vector<double>;
+    virtual void fit(const matrix&) = 0;
 public:
-    virtual void fit(const matrix& X, const targets&, double, double, int) = 0;
+    Regressor() = default;
+    virtual void train(const matrix&, const targets&, const double&, const double&, const std::size_t&) = 0;
+    virtual targets predict(const matrix&) = 0;
+    ~Regressor() = default;
 };
 
 class Classifier {
 private:
     using labels = std::vector<int>;
+    virtual void fit(const matrix&, const labels&) = 0;
 public:
-    virtual void fit(const matrix& X, const labels&, double, double, int) = 0;
+    Classifier() = default;
+    virtual void train(const matrix& X, const labels&, const double&, const double&, const std::size_t&) = 0;
+    virtual labels predict(const matrix&) = 0;
+    ~Classifier() = default;
 };
 
 #endif
