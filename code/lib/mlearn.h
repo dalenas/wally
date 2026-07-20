1b0a5b03a1c384338b0a6ef9e764bb557aec1412
Dalen Smith <dalens@uci.edu>
2026-07-20 13:49:16 -0700

    Started declaring base classes. These will hold pure virtual functions for things such as fit, predict, train, etc. Later, these base classes will be wrapped in a namespace that will be used to create the library. Instead of typing vector<vector> constantly, i ddefined a matrix type and a target/label type to make it consistent. These will be polished later, for now it is just to make things easier. Additionally, probability and statistics functions will be moved to a completely separate library that will likely be in a different repo but imported to use here. Class inheritance will follow and will be tested to make sure everything is being connected properly.

--- code/lib/mlearn.h
+++ code/lib/mlearn.h
@@ -1,3 +1,24 @@
+#ifndef MLEARN_H
+#define MLEARN_H
+
 #include <cmath>
 #include <iostream>
 #include <vector>
+
+using matrix = std::vector<std::vector<double>>;
+
+class Regressor {
+private:
+    using targets = std::vector<double>;
+public:
+    virtual void fit(const matrix& X, const targets&, double, double, int) = 0;
+};
+
+class Classifier {
+private:
+    using labels = std::vector<int>;
+public:
+    virtual void fit(const matrix& X, const labels&, double, double, int) = 0;
+};
+
+#endif
