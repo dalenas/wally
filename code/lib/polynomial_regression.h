dd2f5a866b2579789f36296ff9b431c91ecf4c78
Dalen Smith <dalens@uci.edu>
2026-07-21 16:01:07 -0700

    Committing changes to main dev branch.

--- code/lib/polynomial_regression.h
+++ code/lib/polynomial_regression.h
@@ -1,14 +1,19 @@
+#ifndef POLYNOMIAL_REGRESSION_H
+#define POLYNOMIAL_REGRESSION_H
+
 #include "linear_regression.h"
 
 class PolynomialRegression : public LinearRegression {
 private:
-    int degree;
+    unsigned int degree;
 
-    int num_features(const int&);
-    int choose(const int&, const int&);
-    std::vector<std::vector<int>> pascal_matrix(const int&, const int&);
+    std::size_t count_features(const unsigned int&);
+    unsigned int choose(const unsigned int&, const unsigned int&);
+    matrix<unsigned int> pascal_matrix(const std::size_t&, const std::size_t&);
 public:
-    PolynomialRegression(const int&);
-    std::vector<std::vector<double>> transform(const std::vector<std::vector<double>>&);
+    PolynomialRegression(const unsigned int&);
+    matrix<double> transform(const matrix<double>&);
     ~PolynomialRegression() = default;
 };
+
+#endif
