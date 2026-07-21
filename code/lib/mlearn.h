b39c01739f5f34f1bbdfe1de8a3800295b41f706
Dalen Smith <dalens@uci.edu>
2026-07-20 18:54:51 -0700

    Declared Stats namespace to help with computation and separate it from model class definitions.

--- code/lib/mlearn.h
+++ code/lib/mlearn.h
@@ -5,25 +5,47 @@
 #include <iostream>
 #include <vector>
 
-using matrix = std::vector<std::vector<double>>;
+using std::vector;
+template <typename T>
+using matrix = vector<vector<T>>;
+
+namespace Stats {
+    double mean(const vector<int>&);
+    double mean(const vector<double>&);
+    vector<double> mean(const matrix<int>&);
+    vector<double> mean(const matrix<double>&);
+
+    double var(const vector<int>&);
+    double var(const vector<double>&);
+    vector<double> var(const matrix<int>&);
+    vector<double> var(const matrix<double>&);
+
+    double stdd(const vector<int>&);
+    double stdd(const vector<double>&);
+    vector<double> stdd(const matrix<int>&);
+    vector<double> stdd(const matrix<double>&);
+
+    vector<double> norm(const vector<double>&);
+    matrix<double> norm(const matrix<double>&);
+};
 
 class Regressor {
 private:
-    virtual void fit(const matrix&) = 0;
+    virtual void fit(const matrix<double>&) = 0;
 public:
     Regressor() = default;
-    virtual void train(const matrix&, const std::vector<double>&, const double&, const double&, const std::size_t&) = 0;
-    virtual std::vector<double> predict(const matrix&) = 0;
+    virtual void train(const matrix<double>&, const vector<double>&, const double&, const double&, const std::size_t&) = 0;
+    virtual vector<double> predict(const matrix<double>&) = 0;
     ~Regressor() = default;
 };
 
 class Classifier {
 private:
-    virtual void fit(const matrix&, const std::vector<double>&) = 0;
+    virtual void fit(const matrix<double>&, const vector<double>&) = 0;
 public:
     Classifier() = default;
-    virtual void train(const matrix& X, const std::vector<double>&, const double&, const double&, const std::size_t&) = 0;
-    virtual std::vector<double> predict(const matrix&) = 0;
+    virtual void train(const matrix<double>& X, const vector<double>&, const double&, const double&, const std::size_t&) = 0;
+    virtual vector<double> predict(const matrix<double>&) = 0;
     ~Classifier() = default;
 };
 
