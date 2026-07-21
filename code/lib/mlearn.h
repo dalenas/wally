15aaa125389163cf4a3a546592ff4d7afc2f8c76
Dalen Smith <dalens@uci.edu>
2026-07-20 18:21:06 -0700

    Restored changes to targets and labels naming.

--- code/lib/mlearn.h
+++ code/lib/mlearn.h
@@ -9,23 +9,21 @@ using matrix = std::vector<std::vector<double>>;
 
 class Regressor {
 private:
-    using targets = std::vector<double>;
     virtual void fit(const matrix&) = 0;
 public:
     Regressor() = default;
-    virtual void train(const matrix&, const targets&, const double&, const double&, const std::size_t&) = 0;
-    virtual targets predict(const matrix&) = 0;
+    virtual void train(const matrix&, const std::vector<double>&, const double&, const double&, const std::size_t&) = 0;
+    virtual std::vector<double> predict(const matrix&) = 0;
     ~Regressor() = default;
 };
 
 class Classifier {
 private:
-    using labels = std::vector<int>;
-    virtual void fit(const matrix&, const labels&) = 0;
+    virtual void fit(const matrix&, const std::vector<double>&) = 0;
 public:
     Classifier() = default;
-    virtual void train(const matrix& X, const labels&, const double&, const double&, const std::size_t&) = 0;
-    virtual labels predict(const matrix&) = 0;
+    virtual void train(const matrix& X, const std::vector<double>&, const double&, const double&, const std::size_t&) = 0;
+    virtual std::vector<double> predict(const matrix&) = 0;
     ~Classifier() = default;
 };
 
