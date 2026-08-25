630d2712ea99647597a770b4e19850e784f75865
Dalen Smith <dalens@uci.edu>
2026-08-25 12:50:28 -0700

    Defined cross products by row and by column for classification method predictions and matrix x vector cross products for regression prediction and gradient computation.

--- code/lib/simd.h
+++ code/lib/simd.h
@@ -5,39 +5,43 @@
 
 #include "matrix.h"
 
-alignas(32) constexpr int masks[9][8] = {
-    { 0,  0,  0,  0,  0,  0,  0,  0}, 
-    {-1,  0,  0,  0,  0,  0,  0,  0}, 
-    {-1, -1,  0,  0,  0,  0,  0,  0}, 
-    {-1, -1, -1,  0,  0,  0,  0,  0}, 
-    {-1, -1, -1, -1,  0,  0,  0,  0}, 
-    {-1, -1, -1, -1, -1,  0,  0,  0}, 
-    {-1, -1, -1, -1, -1, -1,  0,  0}, 
-    {-1, -1, -1, -1, -1, -1, -1,  0}, 
-    {-1, -1, -1, -1, -1, -1, -1, -1}
+alignas(32) const __m256i masks[9] = {
+    _mm256_setr_epi32( 0,  0,  0,  0,  0,  0,  0,  0), 
+    _mm256_setr_epi32(-1,  0,  0,  0,  0,  0,  0,  0), 
+    _mm256_setr_epi32(-1, -1,  0,  0,  0,  0,  0,  0), 
+    _mm256_setr_epi32(-1, -1, -1,  0,  0,  0,  0,  0), 
+    _mm256_setr_epi32(-1, -1, -1, -1,  0,  0,  0,  0), 
+    _mm256_setr_epi32(-1, -1, -1, -1, -1,  0,  0,  0), 
+    _mm256_setr_epi32(-1, -1, -1, -1, -1, -1,  0,  0), 
+    _mm256_setr_epi32(-1, -1, -1, -1, -1, -1, -1,  0), 
+    _mm256_setr_epi32(-1, -1, -1, -1, -1, -1, -1, -1)
 };
 
 namespace SIMD {
     namespace SIMD_ {
         __m256 load_k(const float* const, size_t);
+        void store_k(float*, __m256, size_t);
 
         float _mm256_sum_ps(__m256 a);
-        float _mm256_fmsum_ps(__m256 a, __m256 b);
     }
+
+    void zero(Matrix&);
     
     float sum(const Matrix&);
     float dot(const Matrix&);
 
     void add(const Matrix&, const Matrix&, Matrix&);
     void sub(const Matrix&, const Matrix&, Matrix&);
-    void mult(float, const Matrix&, Matrix&);
-    void mult(const Matrix&, const Matrix&, Matrix&);
+    void mul(float, const Matrix&, Matrix&);
+    void mul(const Matrix&, const Matrix&, Matrix&);
 
     void mult_sum();
 
     void mult_comp_add(Matrix&, float, const Matrix&);
-    void matrix_mul(Matrix&, const Matrix&, const Matrix&);
-    void matrix_mul2(Matrix&, const Matrix&, const Matrix&);
+    void cross_mul_bycol(const Matrix&, const Matrix&, Matrix&);        // used for prediction when #features >> #classes => #rows >> #cols
+    void cross_mul_byrow(const Matrix&, const Matrix&, Matrix&);        // used for prediction when #features << #classes => #rows << #cols
+    void cross_mul_to_N(const Matrix&, const Matrix&, Matrix&);         // used for prediction on regression models
+    void cross_mul_to_M(const Matrix&, const Matrix&, Matrix&);         // used for gradient since error vector is 1d
     void mult_comp_sub(Matrix&, float, const Matrix&);
 }
 
--- code/src/simd.cpp
+++ code/src/simd.cpp
@@ -1,21 +1,14 @@
 #include "../lib/simd.h"
 
-using namespace SIMD;
-
-__m256 SIMD_::load(const float* const a) {
-    return _mm256_loadu_ps(a);
+__m256 SIMD::SIMD_::load_k(const float* const a, size_t k) {
+    return _mm256_maskload_ps(a, masks[k]);
 }
 
-__m256 SIMD_::load_k(const float* const a, size_t count) {
-    __m256i mask = _mm256_load_si256(reinterpret_cast<const __m256i*>(masks[count]));
-    return _mm256_maskload_ps(a, mask);
+void SIMD::SIMD_::store_k(float* a, __m256 b, size_t k) {
+    _mm256_maskstore_ps(a, masks[k], b);
 }
 
-void SIMD_::store(Matrix& X, __m256 X_256) {
-
-}
-
-float SIMD_::_mm256_sum_ps(__m256 a) {
+float SIMD::SIMD_::_mm256_sum_ps(__m256 a) {
     __m128 high = _mm256_castps256_ps128(a);
     __m128 low = _mm256_extractf32x4_ps(a, 1);
 
@@ -24,14 +17,10 @@ float SIMD_::_mm256_sum_ps(__m256 a) {
     sum_128 = _mm_hadd_ps(sum_128, sum_128);
     sum_128 = _mm_hadd_ps(sum_128, sum_128);
 
-    float sum = _mm_cvtss_f32(sum_128);
+    return _mm_cvtss_f32(sum_128);
 }
 
-float SIMD_::_mm256_fmsum_ps(__m256 a, __m256 b) {
-    
-}
-
-float sum(const Matrix& X) {
+float SIMD::sum(const Matrix& X) {
     const float* const x = X.data();
 
     size_t i = 0;
@@ -42,18 +31,18 @@ float sum(const Matrix& X) {
         sum_vec = _mm256_add_ps(sum_vec, x_vec);
     }
 
-    float sum = _mm256_sum_ps(sum_vec);
+    float sum = SIMD_::_mm256_sum_ps(sum_vec);
     for(size_t r = i; r < X.size(); ++r)
         sum += x[r];
 
     return sum;
 }
 
-float dot(const Matrix&) {
+float SIMD::dot(const Matrix&) {
 
 }
 
-void add(const Matrix& A, const Matrix& B, Matrix& Y) {
+void SIMD::add(const Matrix& A, const Matrix& B, Matrix& Y) {
     const float* const a = A.data();
     const float* const b = B.data();
     float* const y = Y.data();
@@ -72,7 +61,7 @@ void add(const Matrix& A, const Matrix& B, Matrix& Y) {
         y[r] = a[r] + b[r];
 }
 
-void sub(const Matrix& A, const Matrix& B, Matrix& Y) {
+void SIMD::sub(const Matrix& A, const Matrix& B, Matrix& Y) {
     const float* const a = A.data();
     const float* const b = B.data();
     float* const y = Y.data();
@@ -91,7 +80,7 @@ void sub(const Matrix& A, const Matrix& B, Matrix& Y) {
         y[r] = a[r] - b[r];
 }
 
-void mult(float k, const Matrix& A, Matrix& Y) {
+void SIMD::mul(float k, const Matrix& A, Matrix& Y) {
     const float* const a = A.data();
     float* const y = Y.data();
 
@@ -110,7 +99,7 @@ void mult(float k, const Matrix& A, Matrix& Y) {
         y[r] = k * a[r];
 }
 
-void mult(const Matrix& A, const Matrix& B, Matrix& Y) {
+void SIMD::mul(const Matrix& A, const Matrix& B, Matrix& Y) {
     const float* const a = A.data();
     const float* const b = B.data();
     float* const y = Y.data();
@@ -129,7 +118,7 @@ void mult(const Matrix& A, const Matrix& B, Matrix& Y) {
         y[r] = a[r] * b[r];
 }
 
-void mult_comp_add(Matrix& Y, float k, const Matrix& A) {
+void SIMD::mult_comp_add(Matrix& Y, float k, const Matrix& A) {
     const float* const a = A.data();
     float* const y = Y.data();
 
@@ -149,10 +138,23 @@ void mult_comp_add(Matrix& Y, float k, const Matrix& A) {
         y[r] += k * a[r];
 }
 
-// A is row major, B is column major
-    // Predict per class
-    // for more features, less classes: dataset is row-major, weights are column-major
-void matrix_mul(Matrix& Y, const Matrix& A, const Matrix& B) {
+void SIMD::zero(Matrix& Y) {
+    float* const y = Y.data();
+    
+    const size_t N = Y.size();
+    const size_t remainder = N % 8;
+
+    size_t i = 0;
+    __m256 zero = _mm256_setzero_ps();
+    for(; i + 8 <= N; i += 8)
+        _mm256_storeu_ps(y + i, zero);
+    if(remainder != 0)
+        SIMD_::store_k(y + i, zero, remainder);
+}
+
+void SIMD::cross_mul_byrow(const Matrix& A, const Matrix& B, Matrix& Y) {
+    zero(Y);
+
     const float* const a = A.data();
     const float* const b = B.data();
     float* const y = Y.data();
@@ -161,25 +163,25 @@ void matrix_mul(Matrix& Y, const Matrix& A, const Matrix& B) {
     const size_t M = A.cols();
     const size_t K = B.cols();
 
-    size_t r = M % 8;                        // Remaining feature count that can't create a full __m256 object
-    for(size_t i = 0; i < N; ++i) {          // Per point
-        const float* a_row = a + i*M;
+    size_t remainder = M % 8;
+    for(size_t i = 0; i < N; ++i) {
+        const float* const a_row = a + i*M;
 
-        for(size_t k = 0; k < K; ++k) {      // Per class
-            const float* b_col = b + k*M;
+        for(size_t k = 0; k < K; ++k) {
+            const float* const b_col = b + k*M;
 
             size_t j = 0;
             __m256 y_vec = _mm256_setzero_ps();
-            for(; j + 8 <= M; j += 8) {      // 8 features at a time
-                __m256 a_vec = _mm256_loadu_ps(a_row + j);     // load A(i, j) - A(i, j+7), or the ith datapoint's j-j+7 features
-                __m256 b_vec = _mm256_loadu_ps(b_col + j);     // load B(j, k) - B(j+7, k), or the kth class's j-j+7 weights
+            for(; j + 8 <= M; j += 8) {
+                __m256 a_vec = _mm256_loadu_ps(a_row + j);
+                __m256 b_vec = _mm256_loadu_ps(b_col + j);
 
                  y_vec = _mm256_fmadd_ps(a_vec, b_vec, y_vec);
             }
 
-            if(r != 0) {
-                __m256 a_vec = SIMD_::load_k(a_row + j, r);
-                __m256 b_vec = SIMD_::load_k(b_col + j, r);
+            if(remainder != 0) {
+                __m256 a_vec = SIMD_::load_k(a_row + j, remainder);
+                __m256 b_vec = SIMD_::load_k(b_col + j, remainder);
 
                 y_vec = _mm256_fmadd_ps(a_vec, b_vec, y_vec);
             }
@@ -189,11 +191,9 @@ void matrix_mul(Matrix& Y, const Matrix& A, const Matrix& B) {
     }
 }
 
-// mul2 is temporary name
-    // Predict per feature
-    // weights matrix needs to be row-major
-    // so for less features, more classes: dataset and weight matrices are both row major
-void matrix_mul2(Matrix& Y, const Matrix& A, const Matrix& B) {
+void SIMD::cross_mul_bycol(const Matrix& A, const Matrix& B, Matrix& Y) {
+    zero(Y);
+
     const float* const a = A.data();
     const float* const b = B.data();
     float* const y = Y.data();
@@ -202,17 +202,17 @@ void matrix_mul2(Matrix& Y, const Matrix& A, const Matrix& B) {
     const size_t M = A.cols();
     const size_t K = B.cols();
 
-    size_t r = B.cols() % 8;
-    for(size_t i = 0; i < N; ++i) {          // per point
-        const float* a_row = a + i*M;
+    size_t remainder = B.cols() % 8;
+    for(size_t i = 0; i < N; ++i) {
+        const float* const a_row = a + i*M;
         float* y_row = y + i*K;
 
-        for(size_t j = 0; j < M; ++j) {      // per feature
-            const float* b_row = b + j*K;
+        for(size_t j = 0; j < M; ++j) {
+            const float* const b_row = b + j*K;
 
             size_t k = 0;
             __m256 a_vec = _mm256_set1_ps(*(a_row + j));
-            for(; k + 8 <= K; k += 8) {      // 8 classes at a time
+            for(; k + 8 <= K; k += 8) {
                 __m256 b_vec = _mm256_loadu_ps(b_row + k);
                 __m256 y_vec = _mm256_loadu_ps(y_row + k);
                 
@@ -221,18 +221,88 @@ void matrix_mul2(Matrix& Y, const Matrix& A, const Matrix& B) {
                 _mm256_storeu_ps(y_row + k, y_vec);
             }
 
-            if(r != 0) {
-                __m256 b_vec = SIMD_::load_k(b_row + k, r);
-                __m256 y_vec = SIMD_::load_k(y_row + k, r);
+            if(remainder != 0) {
+                __m256 b_vec = SIMD_::load_k(b_row + k, remainder);
+                __m256 y_vec = SIMD_::load_k(y_row + k, remainder);
 
                 y_vec = _mm256_fmadd_ps(a_vec, b_vec, y_vec);
 
-                SIMD_::store_k(y_row + k, y_vec);         // Write store_k for this
+                SIMD_::store_k(y_row + k, y_vec, remainder);
             }
         }
     }
 }
 
-void mult_comp_sub(Matrix&, float, const Matrix&) {
+void SIMD::cross_mul_to_N(const Matrix& A, const Matrix& B, Matrix& Y) {
+    zero(Y);
+
+    const float* const a = A.data();
+    const float* const b = B.data();
+    float* const y = Y.data();
+
+    const size_t N = A.rows();
+    const size_t M = A.cols();
+
+    size_t remainder = M % 8;
+    for(size_t i = 0; i < N; ++i) {
+        const float* const a_row = a + i*M;
+
+        size_t j = 0;
+        __m256 y_vec = _mm256_setzero_ps();
+        for(; j + 8 <= M; j += 8) {
+            __m256 a_vec = _mm256_loadu_ps(a_row + j);
+            __m256 b_vec = _mm256_loadu_ps(b + j);
+            
+            y_vec = _mm256_fmadd_ps(a_vec, b_vec, y_vec);
+        }
+
+        if(remainder != 0) {
+            __m256 a_vec = SIMD_::load_k(a_row + j, remainder);
+            __m256 b_vec = SIMD_::load_k(b + j, remainder);
+
+            y_vec = _mm256_fmadd_ps(a_vec, b_vec, y_vec);
+        }
+
+        y[i] = SIMD_::_mm256_sum_ps(y_vec);
+    }
+}
+
+void SIMD::cross_mul_to_M(const Matrix& A, const Matrix& B, Matrix& Y) {
+    zero(Y);
+
+    const float* const a = A.data();
+    const float* const b = B.data();
+    float* const y = Y.data();
+
+    const size_t N = A.rows();
+    const size_t M = A.cols();
+
+    size_t remainder = M % 8;
+    for(size_t i = 0; i < N; ++i) {
+        const float* const a_row = a + i*M;
+
+        size_t j = 0;
+        __m256 b_vec = _mm256_set1_ps(*(b + i));
+        for(; j + 8 <= M; j += 8) {
+            __m256 a_vec = _mm256_loadu_ps(a_row + j);
+            __m256 y_vec = _mm256_loadu_ps(y + j);
+
+            y_vec = _mm256_fmadd_ps(a_vec, b_vec, y_vec);
+
+            _mm256_storeu_ps(y + j, y_vec);
+        }
+
+        if(remainder != 0) {
+            __m256 a_vec = SIMD_::load_k(a_row + j, remainder);
+            __m256 y_vec = SIMD_::load_k(y + j, remainder);
+
+            y_vec = _mm256_fmadd_ps(a_vec, b_vec, y_vec);
+
+            SIMD_::store_k(y + j, y_vec, remainder);
+        }
+    }
+}
+
+void SIMD::mult_comp_sub(Matrix&, float, const Matrix&) {
 
 }
