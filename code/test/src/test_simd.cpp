c22a4e790c364c13f421c2ca1a6c209a3a6d7232
Dalen Smith <dalens@uci.edu>
2026-09-16 22:55:37 -0700

    Tested remaining private SIMD functions and everything passed.

--- code/test/src/test_simd.cpp
+++ code/test/src/test_simd.cpp
@@ -291,13 +291,246 @@ TEST(SIMDTestsPrivate, Div) {
         ASSERT_FLOAT_EQ(y4[i], 1.0f);
     }
 }
-/*
-TEST(SIMDTestsPrivate, Sum);
-TEST(SIMDTestsPrivate, Fmadd);
-TEST(SIMDTestsPrivate, Fmsub);
-TEST(SIMDTestsPrivate, Abs);
-TEST(SIMDTestsPrivate, Rcp);
 
+TEST(SIMDTestsPrivate, Sum) {
+    const std::size_t N = 10;
+    const std::size_t REMAINDER = N % SIMD::WIDTH;
+
+    float a[N] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f};
+    int b[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    const float* const p1 = &a[0];
+    const int* const p2 = &b[0];
+
+    auto a_vec = SIMD::SIMD_::_mm256_loadu(p1);
+    auto a_rem = SIMD::SIMD_::_mm256_maskloadu(p1 + SIMD::WIDTH, REMAINDER);
+    auto b_vec = SIMD::SIMD_::_mm256_loadu(p2);
+    auto b_rem = SIMD::SIMD_::_mm256_maskloadu(p2 + SIMD::WIDTH, REMAINDER);
+
+    float sumf = SIMD::SIMD_::_mm256_sum(a_vec);
+    sumf += SIMD::SIMD_::_mm256_sum(a_rem);
+    int sumi = SIMD::SIMD_::_mm256_sum(b_vec);
+    sumi += SIMD::SIMD_::_mm256_sum(b_rem);
+
+    ASSERT_FLOAT_EQ(sumf, 55.0f);
+    ASSERT_EQ(sumi, 55);
+}
+
+TEST(SIMDTestsPrivate, Fmadd) {
+    const std::size_t N = 10;
+    const std::size_t REMAINDER = N % SIMD::WIDTH;
+
+    float a[N] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f};
+    int b[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    const float* const p1 = &a[0];
+    const int* const p2 = &b[0];
+
+    auto a_vec = SIMD::SIMD_::_mm256_loadu(p1);
+    auto a_rem = SIMD::SIMD_::_mm256_maskloadu(p1 + SIMD::WIDTH, REMAINDER);
+    auto b_vec = SIMD::SIMD_::_mm256_loadu(p2);
+    auto b_rem = SIMD::SIMD_::_mm256_maskloadu(p2 + SIMD::WIDTH, REMAINDER);
+
+    auto y1_vec = SIMD::SIMD_::_mm256_fmadd(a_vec, a_vec, a_vec);
+    auto y1_rem = SIMD::SIMD_::_mm256_fmadd(a_rem, a_rem, a_rem);
+    auto y2_vec = SIMD::SIMD_::_mm256_fmadd(a_vec, a_vec, b_vec);
+    auto y2_rem = SIMD::SIMD_::_mm256_fmadd(a_rem, a_rem, b_rem);
+    auto y3_vec = SIMD::SIMD_::_mm256_fmadd(b_vec, b_vec, a_vec);
+    auto y3_rem = SIMD::SIMD_::_mm256_fmadd(b_rem, b_rem, a_rem);
+    auto y4_vec = SIMD::SIMD_::_mm256_fmadd(a_vec, b_vec, b_vec);
+    auto y4_rem = SIMD::SIMD_::_mm256_fmadd(a_rem, b_rem, b_rem);
+    auto y5_vec = SIMD::SIMD_::_mm256_fmadd(b_vec, a_vec, a_vec);
+    auto y5_rem = SIMD::SIMD_::_mm256_fmadd(b_rem, a_rem, a_rem);
+    auto y6_vec = SIMD::SIMD_::_mm256_fmadd(b_vec, a_vec, b_vec);
+    auto y6_rem = SIMD::SIMD_::_mm256_fmadd(b_rem, a_rem, b_rem);
+    auto y7_vec = SIMD::SIMD_::_mm256_fmadd(b_vec, b_vec, a_vec);
+    auto y7_rem = SIMD::SIMD_::_mm256_fmadd(b_rem, b_rem, a_rem);
+    auto y8_vec = SIMD::SIMD_::_mm256_fmadd(b_vec, b_vec, b_vec);
+    auto y8_rem = SIMD::SIMD_::_mm256_fmadd(b_rem, b_rem, b_rem);
+
+    float y1[N];
+    float y2[N];
+    float y3[N];
+    float y4[N];
+    float y5[N];
+    float y6[N];
+    float y7[N];
+    float y8[N];
+    SIMD::SIMD_::_mm256_storeu(y1, y1_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y1 + SIMD::WIDTH, y1_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y2, y2_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y2 + SIMD::WIDTH, y2_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y3, y3_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y3 + SIMD::WIDTH, y3_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y4, y4_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y4 + SIMD::WIDTH, y4_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y5, y5_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y5 + SIMD::WIDTH, y5_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y6, y6_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y6 + SIMD::WIDTH, y6_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y7, y7_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y7 + SIMD::WIDTH, y7_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y8, y8_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y8 + SIMD::WIDTH, y8_rem, REMAINDER);
+
+    for(std::size_t i = 0; i < N; ++i) {
+        float val2 = static_cast<float>(i+1)*static_cast<float>(i+1) + static_cast<float>(i+1);
+
+        ASSERT_FLOAT_EQ(y1[i], val2);
+        ASSERT_FLOAT_EQ(y2[i], val2);
+        ASSERT_FLOAT_EQ(y3[i], val2);
+        ASSERT_FLOAT_EQ(y4[i], val2);
+        ASSERT_FLOAT_EQ(y5[i], val2);
+        ASSERT_FLOAT_EQ(y6[i], val2);
+        ASSERT_FLOAT_EQ(y7[i], val2);
+        ASSERT_FLOAT_EQ(y8[i], val2);
+    }
+}
+
+TEST(SIMDTestsPrivate, Fmsub) {
+    const std::size_t N = 10;
+    const std::size_t REMAINDER = N % SIMD::WIDTH;
+
+    float a[N] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f};
+    int b[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    const float* const p1 = &a[0];
+    const int* const p2 = &b[0];
+
+    auto a_vec = SIMD::SIMD_::_mm256_loadu(p1);
+    auto a_rem = SIMD::SIMD_::_mm256_maskloadu(p1 + SIMD::WIDTH, REMAINDER);
+    auto b_vec = SIMD::SIMD_::_mm256_loadu(p2);
+    auto b_rem = SIMD::SIMD_::_mm256_maskloadu(p2 + SIMD::WIDTH, REMAINDER);
+
+    auto y1_vec = SIMD::SIMD_::_mm256_fmsub(a_vec, a_vec, a_vec);
+    auto y1_rem = SIMD::SIMD_::_mm256_fmsub(a_rem, a_rem, a_rem);
+    auto y2_vec = SIMD::SIMD_::_mm256_fmsub(a_vec, a_vec, b_vec);
+    auto y2_rem = SIMD::SIMD_::_mm256_fmsub(a_rem, a_rem, b_rem);
+    auto y3_vec = SIMD::SIMD_::_mm256_fmsub(b_vec, b_vec, a_vec);
+    auto y3_rem = SIMD::SIMD_::_mm256_fmsub(b_rem, b_rem, a_rem);
+    auto y4_vec = SIMD::SIMD_::_mm256_fmsub(a_vec, b_vec, b_vec);
+    auto y4_rem = SIMD::SIMD_::_mm256_fmsub(a_rem, b_rem, b_rem);
+    auto y5_vec = SIMD::SIMD_::_mm256_fmsub(b_vec, a_vec, a_vec);
+    auto y5_rem = SIMD::SIMD_::_mm256_fmsub(b_rem, a_rem, a_rem);
+    auto y6_vec = SIMD::SIMD_::_mm256_fmsub(b_vec, a_vec, b_vec);
+    auto y6_rem = SIMD::SIMD_::_mm256_fmsub(b_rem, a_rem, b_rem);
+    auto y7_vec = SIMD::SIMD_::_mm256_fmsub(b_vec, b_vec, a_vec);
+    auto y7_rem = SIMD::SIMD_::_mm256_fmsub(b_rem, b_rem, a_rem);
+    auto y8_vec = SIMD::SIMD_::_mm256_fmsub(b_vec, b_vec, b_vec);
+    auto y8_rem = SIMD::SIMD_::_mm256_fmsub(b_rem, b_rem, b_rem);
+
+    float y1[N];
+    float y2[N];
+    float y3[N];
+    float y4[N];
+    float y5[N];
+    float y6[N];
+    float y7[N];
+    float y8[N];
+    SIMD::SIMD_::_mm256_storeu(y1, y1_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y1 + SIMD::WIDTH, y1_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y2, y2_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y2 + SIMD::WIDTH, y2_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y3, y3_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y3 + SIMD::WIDTH, y3_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y4, y4_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y4 + SIMD::WIDTH, y4_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y5, y5_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y5 + SIMD::WIDTH, y5_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y6, y6_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y6 + SIMD::WIDTH, y6_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y7, y7_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y7 + SIMD::WIDTH, y7_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y8, y8_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y8 + SIMD::WIDTH, y8_rem, REMAINDER);
+
+    for(std::size_t i = 0; i < N; ++i) {
+        float val2 = static_cast<float>(i+1)*static_cast<float>(i+1) - static_cast<float>(i+1);
+
+        ASSERT_FLOAT_EQ(y1[i], val2);
+        ASSERT_FLOAT_EQ(y2[i], val2);
+        ASSERT_FLOAT_EQ(y3[i], val2);
+        ASSERT_FLOAT_EQ(y4[i], val2);
+        ASSERT_FLOAT_EQ(y5[i], val2);
+        ASSERT_FLOAT_EQ(y6[i], val2);
+        ASSERT_FLOAT_EQ(y7[i], val2);
+        ASSERT_FLOAT_EQ(y8[i], val2);
+    }
+}
+
+TEST(SIMDTestsPrivate, Abs) {
+    const std::size_t N = 10;
+    const std::size_t REMAINDER = N % SIMD::WIDTH;
+
+    float a[N] = {-1.0f, 2.0f, -3.0f, 4.0f, -5.0f, 6.0f, -7.0f, 8.0f, -9.0f, 10.0f};
+    int b[N] = {-1, 2, -3, 4, -5, 6, -7, 8, -9, 10};
+
+    const float* const p1 = &a[0];
+    const int* const p2 = &b[0];
+
+    auto a_vec = SIMD::SIMD_::_mm256_loadu(p1);
+    auto a_rem = SIMD::SIMD_::_mm256_maskloadu(p1 + SIMD::WIDTH, REMAINDER);
+    auto b_vec = SIMD::SIMD_::_mm256_loadu(p2);
+    auto b_rem = SIMD::SIMD_::_mm256_maskloadu(p2 + SIMD::WIDTH, REMAINDER);
+
+    a_vec = SIMD::SIMD_::_mm256_abs(a_vec);
+    a_rem = SIMD::SIMD_::_mm256_abs(a_rem);
+    b_vec = SIMD::SIMD_::_mm256_abs(b_vec);
+    b_rem = SIMD::SIMD_::_mm256_abs(b_rem);
+
+    float y1[N];
+    int y2[N];
+
+    SIMD::SIMD_::_mm256_storeu(y1, a_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y1 + SIMD::WIDTH, a_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y2, b_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y2 + SIMD::WIDTH, b_rem, REMAINDER);
+
+    for(std::size_t i = 0; i < N; ++i) {
+        ASSERT_FLOAT_EQ(y1[i], static_cast<float>(i+1));
+        ASSERT_EQ(y2[i], static_cast<int>(i + 1));
+    }
+}
+
+TEST(SIMDTestsPrivate, Rcp) {
+    const std::size_t N = 10;
+    const std::size_t REMAINDER = N % SIMD::WIDTH;
+    
+    const float MAX_ERR = 1.5f * 0.000244140625f;    // 1.5*2^-12
+
+    float a[N] = {1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f};
+    int b[N] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    const float* const p1 = &a[0];
+    const int* const p2 = &b[0];
+
+    auto a_vec = SIMD::SIMD_::_mm256_loadu(p1);
+    auto a_rem = SIMD::SIMD_::_mm256_maskloadu(p1 + SIMD::WIDTH, REMAINDER);
+    auto b_vec = SIMD::SIMD_::_mm256_loadu(p2);
+    auto b_rem = SIMD::SIMD_::_mm256_maskloadu(p2 + SIMD::WIDTH, REMAINDER);
+
+    auto y1_vec = SIMD::SIMD_::_mm256_rcp(a_vec);
+    auto y1_rem = SIMD::SIMD_::_mm256_rcp(a_rem);
+    auto y2_vec = SIMD::SIMD_::_mm256_rcp(b_vec);
+    auto y2_rem = SIMD::SIMD_::_mm256_rcp(b_rem);
+
+    float y1[N];
+    float y2[N];
+
+    SIMD::SIMD_::_mm256_storeu(y1, y1_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y1 + SIMD::WIDTH, y1_rem, REMAINDER);
+    SIMD::SIMD_::_mm256_storeu(y2, y2_vec);
+    SIMD::SIMD_::_mm256_maskstoreu(y2 + SIMD::WIDTH, y2_rem, REMAINDER);
+
+    for(std::size_t i = 0; i < N; ++i) {
+        float true_rcp = 1.0f / static_cast<float>(i+1);
+
+        ASSERT_NEAR(y1[i], true_rcp, MAX_ERR);
+        ASSERT_NEAR(y2[i], true_rcp, MAX_ERR);
+    }
+}
+
+/*
 TEST(SIMDTests, Zero);
 TEST(SIMDTests, Sum);
 TEST(SIMDTests, Dot);
